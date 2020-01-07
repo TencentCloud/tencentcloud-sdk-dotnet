6154ecbcf8eb62fb7dbf14a598f7b0e8cb2cabf5
tianhuang <tianhuang@tencent.com>
2020-01-07 10:18:43 +0800

    add sync method to HttpConnection

--- TencentCloud/Common/Http/HttpConnection.cs
+++ TencentCloud/Common/Http/HttpConnection.cs
@@ -17,10 +17,9 @@
 using Pathoschild.Http.Client;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,8 +29,14 @@ namespace TencentCloud.Common.Http
     {
         private IClient client;
 
+        private readonly string proxy;
+
+        private readonly int timeout;
+
         public HttpConnection(string baseUrl, int timeout,string proxy="")
         {
+            this.proxy = proxy;
+            this.timeout = timeout;
             if (!string.IsNullOrEmpty(proxy))
             {
                 client = new FluentClient(baseUrl, new WebProxy(proxy));
@@ -44,17 +49,58 @@ namespace TencentCloud.Common.Http
             client.SetOptions(new FluentClientOptions() { });
         }
 
+        private HttpWebRequest CreateHttp(string url, HttpMethod method)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            request.Timeout = this.timeout * 1000;
+            request.Method = method.Method;
+            if (!string.IsNullOrEmpty(proxy))
+            {
+                request.Proxy = new WebProxy(proxy);
+            }
+            return request;
+        }
+
+        private HttpWebRequest CreateHttp(string url, HttpMethod method, Dictionary<string, string> headers)
+        {
+            HttpWebRequest request = this.CreateHttp(url, method);
+            request.PreAuthenticate = true;
+            string contentTypeName = "Content-Type";
+            // 计算签名时将charset添加到了Content-Type中
+            request.ContentType = headers[contentTypeName] + "; charset=utf-8";
+            headers.Remove(contentTypeName);
+            foreach (KeyValuePair<string, string> kvp in headers)
+            {
+                request.Headers.Add(kvp.Key, kvp.Value);
+            }
+            // 为了和异步接口保持一致，删除headers中的Authorization信息
+            headers.Remove("Authorization");
+            return request;
+        }
+
+        private static string AppendQuery(StringBuilder builder, Dictionary<string, string> param)
+        {
+            foreach (KeyValuePair<string, string> kvp in param)
+            {
+                builder.Append($"{WebUtility.UrlEncode(kvp.Key)}={WebUtility.UrlEncode(kvp.Value)}&");
+            }
+            return builder.ToString().TrimEnd('&');
+        }
+
         public async Task<IResponse> GetRequest(string url, Dictionary<string, string> param)
         {        
             StringBuilder urlBuilder = new StringBuilder($"{client.BaseClient.BaseAddress.AbsoluteUri.TrimEnd('/')}{url}?");
-
-            foreach (KeyValuePair<string, string> kvp in param)
-            {
-                urlBuilder.Append($"{WebUtility.UrlEncode(kvp.Key)}={WebUtility.UrlEncode(kvp.Value)}&");
-            }
-
-            IResponse response = await client.GetAsync(urlBuilder.ToString().TrimEnd('&'));
+            string fullUrl = AppendQuery(urlBuilder, param);
+            IResponse response = await client.GetAsync(fullUrl);
             return response;
+        }
+
+        public HttpWebResponse GetRequestSync(string url, Dictionary<string, string> param)
+        { 
+            StringBuilder urlBuilder = new StringBuilder($"{client.BaseClient.BaseAddress.AbsoluteUri.TrimEnd('/')}{url}?");
+            string fullUrl = AppendQuery(urlBuilder, param);
+            HttpWebRequest request = this.CreateHttp(fullUrl, HttpMethod.Get);
+            return (HttpWebResponse)request.GetResponse();
         }
 
         public async Task<IResponse> GetRequest(string path, string queryString, Dictionary<string, string> headers)
@@ -73,6 +119,13 @@ namespace TencentCloud.Common.Http
             return await request;
         }
 
+        public HttpWebResponse GetRequestSync(string path, string queryString, Dictionary<string, string> headers)
+        {
+            StringBuilder urlBuilder = new StringBuilder($"{client.BaseClient.BaseAddress.AbsoluteUri.TrimEnd('/')}{path}?{queryString}");
+            HttpWebRequest request = this.CreateHttp(urlBuilder.ToString(), HttpMethod.Get, headers);
+            return (HttpWebResponse)request.GetResponse();
+        }
+
         public async Task<IResponse> PostRequest(string path, string payload, Dictionary<string, string> headers)
         {
             StringBuilder urlBuilder = new StringBuilder($"{client.BaseClient.BaseAddress.AbsoluteUri.TrimEnd('/')}{path}");
@@ -89,6 +142,18 @@ namespace TencentCloud.Common.Http
             return await request.AsResponse();
         }
 
+        public HttpWebResponse PostRequestSync(string path, string payload, Dictionary<string, string> headers)
+        {
+            StringBuilder urlBuilder = new StringBuilder($"{client.BaseClient.BaseAddress.AbsoluteUri.TrimEnd('/')}{path}");
+            HttpWebRequest request = this.CreateHttp(urlBuilder.ToString(), HttpMethod.Post, headers);
+            using (Stream reqStream = request.GetRequestStream())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(payload);
+                reqStream.Write(bytes, 0, bytes.Length);
+            }
+            return (HttpWebResponse)request.GetResponse();
+        }
+
         public async Task<IResponse> PostRequest(string url, Dictionary<string, string> param)
         {
             // set up
@@ -97,15 +162,29 @@ namespace TencentCloud.Common.Http
             // System.UriFormatException: Invalid URI: The Uri string is too long.
             // var postbody = new FormUrlEncodedContent(param);
             StringBuilder bodysb = new StringBuilder();
-            foreach (KeyValuePair<string, string> kvp in param)
-            {
-                bodysb.Append($"{WebUtility.UrlEncode(kvp.Key)}={WebUtility.UrlEncode(kvp.Value)}&");
-            }
-            message.Content = new StringContent(bodysb.ToString().TrimEnd('&'), Encoding.UTF8, "application/x-www-form-urlencoded");
+            string body = AppendQuery(bodysb, param);
+            message.Content = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded");
 
             IRequest request =  client.SendAsync(message);
             var response = await request.AsResponse();
             return response;
+        }
+
+        public HttpWebResponse PostRequestSync(string url, Dictionary<string, string> param)
+        {
+            Dictionary<string, string> headers = new Dictionary<string, string>
+            {
+                ["Content-Type"] = "application/x-www-form-urlencoded"
+            };
+            HttpWebRequest request = this.CreateHttp(url, HttpMethod.Post, headers);
+            StringBuilder bodysb = new StringBuilder();
+            string body = AppendQuery(bodysb, param);
+            using (Stream reqStream = request.GetRequestStream())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(body);
+                reqStream.Write(bytes, 0, bytes.Length);
+            }
+            return (HttpWebResponse)request.GetResponse();
         }
     }
 }
