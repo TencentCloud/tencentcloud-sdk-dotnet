1f02fe65dbd00a4e467b21b292f9211f6edddc3b
zhiqiangfan <zhiqiangfan@tencent.com>
2021-10-21 16:03:05 +0800

    update example

--- TencentCloudExamples/DescribeInstancesSync.cs
+++ TencentCloudExamples/DescribeInstancesSync.cs
@@ -42,7 +42,7 @@ namespace TencentCloudExamples
                 // 实例化一个client选项，可选的，没有特殊需求可以跳过
                 ClientProfile clientProfile = new ClientProfile();
                 // 指定签名算法(默认为HmacSHA256)
-                clientProfile.SignMethod = ClientProfile.SIGN_SHA256;
+                clientProfile.SignMethod = ClientProfile.SIGN_TC3SHA256;
                 // 非必要步骤
                 // 实例化一个客户端配置对象，可以指定超时时间等配置
                 HttpProfile httpProfile = new HttpProfile();
