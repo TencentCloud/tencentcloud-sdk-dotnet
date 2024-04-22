d07f416e996bd883cfb715156963cfe96e641940
sesky4 <sesky4@gmail.com>
2024-04-22 16:34:28 +0800

    Update ChatStd.cs

--- TencentCloudExamples/ChatStd.cs
+++ TencentCloudExamples/ChatStd.cs
@@ -9,19 +9,7 @@ namespace TencentCloudExamples
 {
     class ChatStd
     {
-        static void Main(string[] args)
-        {
-            // hunyuan ChatStd/ChatPro 同时支持 stream 和非 stream 的情况
-
-            // stream 示例
-            Stream();
-            
-            // 非 stream 示例
-            // 通过 Stream=false 参数来指定非 stream 协议, 一次性拿到结果
-            NonStream();
-        }
-
-        static void Stream()
+        static void MainOfChatStd(string[] args)
         {
             Credential cred = new Credential
             {
@@ -44,39 +32,22 @@ namespace TencentCloudExamples
                 }
             };
 
+            // hunyuan ChatStd/ChatPro 同时支持 stream 和非 stream 的情况
+            req.Stream = true;
+
             var resp = client.ChatStdSync(req);
-            foreach (var e in resp)
+            if (req.Stream.HasValue && req.Stream.Value)
             {
-                Console.WriteLine(e.Data);
+                // stream 示例
+                foreach (var e in resp)
+                    Console.WriteLine(e.Data);
             }
-        }
-
-        static void NonStream()
-        {
-            Credential cred = new Credential
+            else
             {
-                SecretId = Environment.GetEnvironmentVariable("TENCENTCLOUD_SECRET_ID"),
-                SecretKey = Environment.GetEnvironmentVariable("TENCENTCLOUD_SECRET_KEY")
-            };
-
-            var cpf = new ClientProfile();
-
-            var client = new HunyuanClient(cred, "ap-guangzhou", cpf);
-            var req = new ChatStdRequest
-            {
-                Messages = new[]
-                {
-                    new Message
-                    {
-                        Role = "user",
-                        Content = "你好，可以讲个笑话吗",
-                    }
-                },
-                Stream = false,
-            };
-
-            var resp = client.ChatStdSync(req);
-            Console.WriteLine(JsonConvert.SerializeObject(resp));
+                // 非 stream 示例
+                // 通过 Stream=false 参数来指定非 stream 协议, 一次性拿到结果
+                Console.WriteLine(JsonConvert.SerializeObject(resp));
+            }
         }
     }
 }
