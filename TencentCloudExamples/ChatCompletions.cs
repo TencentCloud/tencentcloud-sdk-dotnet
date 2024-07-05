c201de31484b10d19ed44ca87d1a87a015703dbf
邹文杰 <72617766+19916302128@users.noreply.github.com>
2024-07-05 11:28:43 +0800

    Update and rename ChatStd.cs to ChatCompletions.cs

--- TencentCloudExamples/ChatCompletions.cs
+++ TencentCloudExamples/ChatCompletions.cs
@@ -7,9 +7,9 @@ using TencentCloud.Hunyuan.V20230901.Models;
 
 namespace TencentCloudExamples
 {
-    class ChatStd
+    class ChatCompletions
     {
-        static void MainOfChatStd(string[] args)
+        static void MainOfChatCompletions(string[] args)
         {
             Credential cred = new Credential
             {
@@ -20,7 +20,7 @@ namespace TencentCloudExamples
             var cpf = new ClientProfile();
 
             var client = new HunyuanClient(cred, "ap-guangzhou", cpf);
-            var req = new ChatStdRequest
+            var req = new ChatCompletionsRequest
             {
                 Messages = new[]
                 {
@@ -32,10 +32,10 @@ namespace TencentCloudExamples
                 }
             };
 
-            // hunyuan ChatStd/ChatPro 同时支持 stream 和非 stream 的情况
+            // hunyuan ChatCompletions/ChatPro 同时支持 stream 和非 stream 的情况
             req.Stream = true;
 
-            var resp = client.ChatStdSync(req);
+            var resp = client.ChatCompletionsSync(req);
             if (req.Stream.HasValue && req.Stream.Value)
             {
                 // stream 示例
