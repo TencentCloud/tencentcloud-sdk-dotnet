3b898fafdbfe0669036998789ac6d859a9d1511e
tencentcloudapi <tencentcloudapi@tencent.com>
2024-12-27 16:46:56 +0800

    release 3.0.1152

--- TencentCloud/Bh/V20230418/BhClient.cs
+++ TencentCloud/Bh/V20230418/BhClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Bh.V20230418
 
        private const string endpoint = "bh.tencentcloudapi.com";
        private const string version = "2023-04-18";
-       private const string sdkVersion = "SDK_NET_3.0.1151";
+       private const string sdkVersion = "SDK_NET_3.0.1152";
 
         /// <summary>
         /// Client constructor.
--- TencentCloud/Tsw/V20210412/TswClient.cs
+++ TencentCloud/Tsw/V20210412/TswClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Tsw.V20210412
 
        private const string endpoint = "tsw.tencentcloudapi.com";
        private const string version = "2021-04-12";
-       private const string sdkVersion = "SDK_NET_3.0.1151";
+       private const string sdkVersion = "SDK_NET_3.0.1152";
 
         /// <summary>
         /// Client constructor.
