2777e6541171593e68e3a19d2f20d093d28c4ab0
sesky4 <sesky4@gmail.com>
2025-08-19 22:21:45 +0800

    test: add tests for empty token (#322)

---
+++ TencentCloud.Tests/TempCredentialTest.cs
@@ -0,0 +1,48 @@
+﻿using TencentCloud.Common;
+using TencentCloud.Common.Profile;
+using TencentCloud.Cvm.V20170312;
+using TencentCloud.Cvm.V20170312.Models;
+
+namespace TencentCloud.Tests
+{
+    [TestClass]
+    public class TempCredentialTest
+    {
+        [TestMethod]
+        public void EmptyToken()
+        {
+            var reqMethods = new string[] { "GET", "POST" };
+            var signMethods = new string[]
+                { ClientProfile.SIGN_SHA1, ClientProfile.SIGN_SHA256, ClientProfile.SIGN_TC3SHA256 };
+            var tokens = new string[] { "", null };
+
+            var combinations =
+                from reqMethod in reqMethods
+                from signMethod in signMethods
+                from token in tokens
+                select new { reqMethod, signMethod, token };
+
+
+            foreach (var c in combinations)
+            {
+                var cred = new Credential
+                {
+                    SecretId = Environment.GetEnvironmentVariable("TENCENTCLOUD_SECRET_ID"),
+                    SecretKey = Environment.GetEnvironmentVariable("TENCENTCLOUD_SECRET_KEY"),
+                    Token = c.token,
+                };
+
+                var cpf = new ClientProfile(c.signMethod)
+                {
+                    HttpProfile =
+                    {
+                        ReqMethod = c.reqMethod
+                    }
+                };
+
+                var client = new CvmClient(cred, "ap-guangzhou", cpf);
+                client.DescribeInstancesSync(new DescribeInstancesRequest());
+            }
+        }
+    }
+}
