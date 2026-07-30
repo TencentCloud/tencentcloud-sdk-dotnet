1fe781401019ae4aa27f512830caf068a998af94
tencentcloudapi <tencentcloudapi@tencent.com>
2026-07-30 16:28:27 +0800

    release 3.0.1475

--- TencentCloud/Common/AbstractClient.cs
+++ TencentCloud/Common/AbstractClient.cs
@@ -42,7 +42,7 @@ namespace TencentCloud.Common
         /// <summary>
         ///   Current SDK version.
         /// </summary>
-        public const string SDK_VERSION = "SDK_NET_3.0.1474";
+        public const string SDK_VERSION = "SDK_NET_3.0.1475";
 
         /// <summary>
         ///   Constructor for AbstractClient.
